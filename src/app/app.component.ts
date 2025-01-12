6d929eaaa7b3b6135b369a3dd76f543ba6f75a67
nattanan_m <nattanan_m@softsquaregroup.com>
2025-01-12 18:43:34 +0700

    edit button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'born2bdev';
+  linkToFastwork(){
+    window.location.href = 'https://fastwork.co/user/born2bdev';
+  }
 }
